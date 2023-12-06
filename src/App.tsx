67ce3b7d602c31fd8d9ebfab74bc27b87ae98730
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-12-06 11:14:05 +0530

    second

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 
 import './App.css'
+import {useState} from "react";
 
 function App() {
+
+    const [value,setValue] =useState("");
+    const [checked,setChecked] = useState(true);
 
 
   return (
     <>
         <h1>React Forms</h1>
-        <input type="text"/>
+        <label><input type="checkbox" checked={checked} onChange={e=>setChecked(e.target.checked)}/>Enable</label><br/>
+        <input type="text" value={value} disabled={!checked} onChange={e =>setValue(e.target.value)}/>
+        <h3>Out Put</h3>
+        <div>{value}</div>
     </>
   )
 }
